f8092766266f02650af2e403f765acbd75b4c9c7
Roger Coll <rogercoll@protonmail.com>
2025-10-20 21:53:20 +0200

    fix brightnessctl and wallpaper

--- dot_config/i3/scripts/executable_volume_brightness.sh
+++ dot_config/i3/scripts/executable_volume_brightness.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# original source: https://gitlab.com/Nmoleo/i3-volume-brightness-indicator
-
-# taken from here: https://gitlab.com/Nmoleo/i3-volume-brightness-indicator
 
 # See README.md for usage instructions
 bar_color="#7f7fff"
 volume_step=1
-brightness_step=2.5
+brightness_step=5
 max_volume=100
 
 # Uses regex to get volume from pactl
@@ -19,9 +16,9 @@ function get_mute {
     pactl get-sink-mute @DEFAULT_SINK@ | grep -Po '(?<=Mute: )(yes|no)'
 }
 
-# Uses regex to get brightness from xbacklight
+# Gets brightness percentage using brightnessctl
 function get_brightness {
-    xbacklight | grep -Po '[0-9]{1,3}' | head -n 1
+    brightnessctl -m | awk -F, '{print $4}' | tr -d '%'
 }
 
 # Returns a mute icon, a volume-low icon, or a volume-high icon, depending on the volume
@@ -44,7 +41,7 @@ function get_brightness_icon {
 
 # Displays a volume notification using dunstify
 function show_volume_notif {
-    volume=$(get_mute)
+    volume=$(get_volume)
     get_volume_icon
     dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal "$volume_icon $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
 }
@@ -53,44 +50,40 @@ function show_volume_notif {
 function show_brightness_notif {
     brightness=$(get_brightness)
     get_brightness_icon
-    dunstify -t 1000 -r 2593 -u normal "$brightness_icon $brightness%" -h int:value:$brightness -h string:hlcolor:$bar_color
+    dunstify -t 1000 -r 2594 -u normal "$brightness_icon $brightness%" -h int:value:$brightness -h string:hlcolor:$bar_color
 }
 
 # Main function - Takes user input, "volume_up", "volume_down", "brightness_up", or "brightness_down"
 case $1 in
     volume_up)
-    # Unmutes and increases volume, then displays the notification
-    pactl set-sink-mute @DEFAULT_SINK@ 0
-    volume=$(get_volume)
-    if [ $(( "$volume" + "$volume_step" )) -gt $max_volume ]; then
-        pactl set-sink-volume @DEFAULT_SINK@ $max_volume%
-    else
-        pactl set-sink-volume @DEFAULT_SINK@ +$volume_step%
-    fi
-    show_volume_notif
-    ;;
+        pactl set-sink-mute @DEFAULT_SINK@ 0
+        volume=$(get_volume)
+        if [ $(( "$volume" + "$volume_step" )) -gt $max_volume ]; then
+            pactl set-sink-volume @DEFAULT_SINK@ $max_volume%
+        else
+            pactl set-sink-volume @DEFAULT_SINK@ +$volume_step%
+        fi
+        show_volume_notif
+        ;;
 
     volume_down)
-    # Raises volume and displays the notification
-    pactl set-sink-volume @DEFAULT_SINK@ -$volume_step%
-    show_volume_notif
-    ;;
+        pactl set-sink-volume @DEFAULT_SINK@ -$volume_step%
+        show_volume_notif
+        ;;
 
     volume_mute)
-    # Toggles mute and displays the notification
-    pactl set-sink-mute @DEFAULT_SINK@ toggle
-    show_volume_notif
-    ;;
+        pactl set-sink-mute @DEFAULT_SINK@ toggle
+        show_volume_notif
+        ;;
 
     brightness_up)
-    # Increases brightness and displays the notification
-    xbacklight -inc $brightness_step -time 0
-    show_brightness_notif
-    ;;
+        brightnessctl set +${brightness_step}%
+        show_brightness_notif
+        ;;
 
     brightness_down)
-    # Decreases brightness and displays the notification
-    xbacklight -dec $brightness_step -time 0
-    show_brightness_notif
-    ;;
+        brightnessctl set ${brightness_step}%-
+        show_brightness_notif
+        ;;
 esac
+
